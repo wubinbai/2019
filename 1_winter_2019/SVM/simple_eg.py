c6ee4d0be531e36aae19bfb9d8b6d23025d66699
wb <wubinbai@yahoo.com>
2019-03-08 18:13:58 +0800

    added simple_eg.py in SVM

--- 1_winter_2019/SVM/simple_eg.py
+++ 1_winter_2019/SVM/simple_eg.py
@@ -1,6 +1,7 @@
 import numpy as np
-x = np.array([[0,1],[1,0],[0,2]])
-y = np.array([1,0,1])
+import matplotlib.pyplot as plt
+x = np.array([[0,1],[1,0],[0,2],[1,0.8],[4,0.5],[2,0.5]])
+y = np.array([1,0,1,1,0,0])
 from sklearn.svm import LinearSVC
 from sklearn import svm
 
@@ -9,3 +10,10 @@ svm_clf.fit(x,y)
 clf = svm.SVC(kernel='linear')
 clf.fit(x,y)
 
+x_linspace = np.linspace(0,10,20)
+y_line = ( - svm_clf.intercept_[0] - x_linspace * svm_clf.coef_[0][0] ) / svm_clf.coef_[0][1] 
+plt.figure()
+plt.scatter(x_linspace,y_line,marker='*')
+plt.scatter(x[:,0],x[:,1],marker='o',color='y')
+plt.ion()
+plt.show()
