223752490379cf2f2241ae84701654a6ab8c4aaa
wb <wubinbai@yahoo.com>
2019-03-21 10:07:43 +0800

    freesound extensible and 2.2 mnist

--- 1_winter_2019/book/Learning_TensorFlow/2.2_MNIST.py
+++ 1_winter_2019/book/Learning_TensorFlow/2.2_MNIST.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+from keras.datasets import mnist
+(X_train, y_train), (X_test, y_test)= mnist.load_data()
 
 DATA_DIR = '/tmp/data'
 NUM_STEPS = 1000
 MINIBATCH_SIZE = 100
 
-data = input_data.read_data_sets(DATA_DIR, one_hot = True)
-"""0
+#data = input_data.read_data_sets(DATA_DIR, one_hot = True)
+
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784,10]))
 
-y_true = tf.placeholder(tf.float32, [None, 784])
+y_true = tf.placeholder(tf.float32, [None, 10])
 y_pred = tf.matmul(x, W)
  
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_pred, labels = y_true))
 
-gd_sterp = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+gd_ster = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
 correct_mask = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_mask, tf.float32))
 
@@ -23,11 +26,11 @@ with tf.Session() as sess:
     # Train
     sess.run(tf.global_variables_initializer())
     for _ in range(NUM_STEPS):
-        batch_xs, batch_ys = data.train.next_batch(MINIBATCH_SIZE)
+        batch_xs, batch_ys = X_train0.next_batch(MINIBATCH_SIZE)
         sess.run(gd_step, feed_dict = {x: batch_xs, y_true: batch_ys})
     
     # Test
     ans = sess.run(accuracy, feed_dict = {x:data.test.images, y_true: data.test.labels})
 
 print("Accuracy: {:.4}%".format(ans*100))
-"""
+
