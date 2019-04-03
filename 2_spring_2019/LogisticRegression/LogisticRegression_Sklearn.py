70f702b51be09b2ce7022b35f5c3747013b4191e
Wubin Bai <wubinbai@yahoo.com>
2019-04-03 11:06:33 +0800

    modified LogisticRegression_Sklearn.py

--- 2_spring_2019/LogisticRegression/LogisticRegression_Sklearn.py
+++ 2_spring_2019/LogisticRegression/LogisticRegression_Sklearn.py
@@ -1,10 +1,15 @@
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+import matplotlib.pyplot as plt
+
 data = np.loadtxt('ex2data1.txt',delimiter=',')
 X = data[:, 0:2]
 y = data[:, 2]
+fig=plt.figure()
+plt.scatter(X[:,0],X[:,1])
+plt.show()
 
 LR = LogisticRegression()
 LR.fit(X,y)
 
-
+print("LR comes from LogisticRegression.fit(X,y)")
