cb7297d812215d29d77e794412913a89e7531004
Wubin Bai <wubinbai@yahoo.com>
2019-07-25 09:08:52 +0800

    things to execute after installing ubuntu

---
+++ ubuntu_related/after_installing_ubuntu.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# First you may wanna follow the instructions on the welcome page. Options are to set up the livepatch: U.s.e.R. : wubinbai e.m.a.i.1: wubinbai@yahoo.com p.s.w: b**00****
+
+# Then setup font manually. Open a terminal,  go to the preference and text, set the font size to around 42-46.
+
+# Then use sudo apt-get update for updating the apt-get
+sudo apt-get update
+
+# Then you can install vim
+
+sudo apt-get install vim
+
+
+
+
+
