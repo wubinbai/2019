e2e7626053939b935ee2e8496120c5e208943305
Wubin Bai <wubinbai@yahoo.com>
2019-07-25 21:25:02 +0800

    anaconda install

--- ubuntu_related/after_installing_ubuntu.sh
+++ ubuntu_related/after_installing_ubuntu.sh
@@ -20,4 +20,29 @@ sudo apt-get install git
 git clone https://github.com/wubinbai/2019.git
 
 
+# Then, to install anaconda. The fastest way to download is using Thunderstorm on Windows Platform, which takes about 3 - 5 mins.
+
+# Prerequisites for installing, which takes about 20 seconds:
+
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+# Then, go to the anaconda.sh you have downloaded, and run the ./xxx.sh
+
+# The installer prompts “In order to continue the installation process, please review the license agreement.” PRESS ENTER to view license terms.
+
+# Scroll to the bottom, ENTER "yes"
+
+# The installer prompts you to PRESS ENTER to accept the default install location. # We recommend you accept the default install location. Do not choose the path as /usr for the Anaconda/Miniconda installation.
+
+# The installer prompts “Do you wish the installer to initialize Anaconda3 by running conda init?” We RECOMMEND “yes”.
+
+# Then, all done. Anaconda and JetBrains are working together to bring you Anaconda-powered environments tightly integrated in the PyCharm IDE.
+
+# Close and open your terminal window for the installation to take effect, OR you can enter the command source ~/.bashrc.
+
+# To control whether or not each shell session has the base environment activated or not, run conda config --set auto_activate_base False or True. To run conda from anywhere without having the base environment activated by default, use conda config --set auto_activate_base False. This only works if you have run conda init first.
+
+# After your install is complete, verify it by opening Anaconda Navigator, a program that is included with Anaconda: Open a terminal window and type anaconda-navigator. If Navigator opens, you have successfully installed Anaconda. If not, check that you completed each step above, then see our Help page.
+
+
 
