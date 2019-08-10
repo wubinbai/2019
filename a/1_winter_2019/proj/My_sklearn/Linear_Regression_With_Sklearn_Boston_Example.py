bc1ccfea0c152097a8749a6a4d4f098f204471af
Wubin Bai <wubinbai@yahoo.com>
2019-08-10 18:13:05 +0800

    boston

--- a/1_winter_2019/proj/My_sklearn/Linear_Regression_With_Sklearn_Boston_Example.py
+++ a/1_winter_2019/proj/My_sklearn/Linear_Regression_With_Sklearn_Boston_Example.py
@@ -8,9 +8,52 @@ df_x = pd.DataFrame(boston.data,columns = boston.feature_names)
 df_y=pd.DataFrame(boston.target)
 df_x.describe()
 reg=linear_model.LinearRegression()
-x_train,x_test,y_train,y_test=train_test_split(df_x,df_y,test_size=0.2,random_state=4)
-x_train.head()
-reg.fit(x_train,y_train)
+X_train,X_test,y_train,y_test=train_test_split(df_x,df_y,test_size=0.2,random_state=4)
+reg.fit(X_train,y_train)
 reg.coef_
-reg.predict(x_test)
-print('accuracy: ', np.mean((reg.predict(x_test)-y_test)**2))
+y_pred=reg.predict(X_test)
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_pred,y_test)
+rmse = np.sqrt(mse)
+print('log_reg rmse:', rmse)
+
+# Try another model, in page 69 of hands on ML
+
+from sklearn.tree import DecisionTreeRegressor
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(X_train,y_train)
+tree_pred = tree_reg.predict(X_test)
+tree_rmse = np.sqrt(mean_squared_error(tree_pred,y_test))
+print('DTR rmse', tree_rmse)
+
+# Better eval using cross val score
+
+from sklearn.model_selection import cross_val_score as crvs
+scores = crvs(tree_reg,df_x,df_y,scoring="neg_mean_squared_error",cv=10)
+rmse_scores = np.sqrt(-scores)
+
+def display_scores(scores):
+    print('======')
+    
+    print('Scores: ', scores)
+    print("Mean: ", scores.mean())
+    print("STD: ", scores.std())
+    print('==end==')
+display_scores(rmse_scores)
+
+# display lin reg scores
+lin_scores = crvs(reg,df_x,df_y,scoring="neg_mean_squared_error",cv=10)
+lin_scores = np.sqrt(-lin_scores)
+display_scores(lin_scores)
+
+
+# Try one more RFR.
+
+from sklearn.ensemble import RandomForestRegressor as RFR
+rfr = RFR()
+
+rfr_scores = crvs(rfr,df_x,np.ravel(df_y),scoring="neg_mean_squared_error",cv=10)
+rfr_scores = np.sqrt(-rfr_scores)
+display_scores(rfr_scores)
+
+
