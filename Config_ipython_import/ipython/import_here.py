22923f1cf5a07d6ff59532827ebf7b53c0a6d0bb
Wubin Bai <wubinbai@yahoo.com>
2019-08-15 15:50:26 +0800

    karting secrets and ipy

--- Config_ipython_import/ipython/import_here.py
+++ Config_ipython_import/ipython/import_here.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 def plot_whole(df):
     with pd.option_context('display.max_rows', None, 'display.max_columns', None): 
         print(df)
