90969b5a4dd22893aa17b508468cf83a00843b44
Wubin Bai <wubinbai@yahoo.com>
2019-10-28 16:50:24 +0800

    notebooks html housing 1nn caveat

--- Kaggle_Light/housing_prices/Linear_Models_Wubin/naive_linear.py
+++ Kaggle_Light/housing_prices/Linear_Models_Wubin/naive_linear.py
@@ -1,8 +1,6 @@
-# There is a bug in this code:
-# new_test data need to fllow procedure as new_train!
+train = pd.read_csv('../input/train.csv')
+test = pd.read_csv("../input/test.csv")
 
-train = pd.read_csv("input/train.csv")
-test = pd.read_csv("input/test.csv")
 
 print(train.shape)
 #print(train.columns)
@@ -69,59 +67,20 @@ from sklearn.model_selection import train_test_split
 train_data, val_data, train_label, val_label = train_test_split(x_train,y_train)
 
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-EPOCHS = 1000
 
 
-m = Sequential()
-m.add(Dense(64,activation='relu',input_shape = x_train.loc[0].shape))
-m.add(Dense(64,activation='relu'))
-m.add(Dense(1))
 
-m.summary()
-
-m.compile(optimizer=keras.optimizers.RMSprop(),loss='mse',metrics=['mae'])
-
-
-history = m.fit(train_data,train_label,epochs=EPOCHS,batch_size=128, validation_data=(val_data,val_label))
-
-pred = pd.DataFrame(m.predict(x_test))
+# Model Def
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(train_data,train_label)
+pred = lr.predict(x_test)
 # scale pred back, because our input labels had been scaled.
 pred *= 1e4
+
 df2 = pd.concat([new_test.Id,pred],axis=1)
 df = df2.set_index('Id',drop=True)
-df.columns = ['SalePrice']     
-temp_mean = df.mean(numeric_only=True) 
+df.columns = ['SalePrice']
+temp_mean = df.mean(numeric_only=True)
 df[df.isnull()] = temp_mean.values[0]
 df.to_csv('sub.csv')
-
-
-
-
-mae = history.history['mean_absolute_error']
-val_mae = history.history['val_mean_absolute_error']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1, len(mae) + 1)
-
-# "bo" is for "blue dot"
-plt.plot(epochs, loss, 'bo', label='Training loss')
-# b is for "solid blue line"
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-plt.figure()
-plt.plot(epochs, mae, 'bo', label='Training mae')
-plt.plot(epochs, val_mae, 'b', label='Validation mae')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('MAE')
-plt.legend()
-plt.show()
