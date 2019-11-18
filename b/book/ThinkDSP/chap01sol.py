696df5d240dd3d1f7d4f3e84131b9502188e6232
Wubin Bai <wubinbai@yahoo.com>
2019-11-18 11:26:30 +0800

    p25 ex2 thinkdsp chap01

--- b/book/ThinkDSP/chap01sol.py
+++ b/book/ThinkDSP/chap01sol.py
@@ -1,3 +1,9 @@
 import thinkdsp
-f = thinkdsp.read_wave('sample.wav')
+f = thinkdsp.read_wave('chords_sample.wav')
+seg = f.segment(duration=0.5)
+ss = seg.make_spectrum()
+ss.plot(high=1500)
+ss.low_pass(cutoff=600)
+nw = ss.make_wave()
+nw.play()
 
