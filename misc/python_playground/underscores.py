cf16983f2f04d415b4d7804d499fd701b12242fe
Wubin Bai <wubinbai@yahoo.com>
2019-12-16 08:06:50 +0800

    underscores

---
+++ misc/python_playground/underscores.py
@@ -0,0 +1,7 @@
+_leading = 'leading underscore'
+trailing_ = 'trailing underscore'
+_both_ = 'both underscore'
+__double__ = 'double underscore'
+__twoleading = 'two leading'
+twotrailing__ = 'two trailing'
+
