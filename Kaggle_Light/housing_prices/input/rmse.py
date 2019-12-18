5507561f0c2ed73b9205c2f077f6c3fa6a096795
wb <wubinbai@yahoo.com>
2019-12-18 12:56:51 +0800

    rmse_cv

---
+++ Kaggle_Light/housing_prices/input/rmse.py
@@ -0,0 +1,6 @@
+
+from sklearn.model_selection import cross_val_score
+
+def rmse_cv(model, X, y):
+    rmse = np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5))
+    return rmse
