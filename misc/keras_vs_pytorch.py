6b6484929fe59bf27377f9777b7bf72673128169
wb <wubinbai@yahoo.com>
2020-01-21 11:23:31 +0800

    keras vs pytorch

--- misc/keras_vs_pytorch.py
+++ misc/keras_vs_pytorch.py
@@ -2,13 +2,15 @@
 
 Keras
 
+from keras.layers import *
+from keras.models import Sequential
 model = Sequential()
 
-model.add(Conv2D( 32, ( 3, 3), activation= 'relu', input_shape=( 32, 32, 3)))
+model.add(Conv2D( 32, ( 3, 3), activation= 'relu`', input_shape=( 32, 32, 3)))
 
 model.add(MaxPool2D())
 
-model.add(Conv2D( 16, ( 3, 3), activation= 'relu'))
+model.add(Conv2D( 16, ( 3, 3), actiivation= 'relu'))
 
 model.add(MaxPool2D())
 
