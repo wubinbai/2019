ef1ee4cc4c4059450288a52de90dd7ac2a3b91d3
Wubin <wubinbai@yahoo.com>
2020-03-12 14:42:51 +0800

    run 8

--- b/book/deep_learning_with_python/6_rnn_etc_12_runs.py
+++ b/book/deep_learning_with_python/6_rnn_etc_12_runs.py
@@ -695,4 +695,84 @@ if run6=='y':
     plt.show()
 
 
+if run7=='y':
+    from keras import layers
+    # Number of words to consider as features
+    max_features = 10000
+    # Cut texts after this number of words (among top max_features most common words)
+    maxlen = 500
+
+    #### load imdb ####
+    print('Loading data...')
+    #(input_train, y_train), (input_test, y_test) = imdb.load_data(path='/home/wb/baidunetdiskdownload/imdb.npz',num_words=max_features)
+
+    import numpy as np
+    # save np.load
+    np_load_old = np.load
+
+    # modify the default parameters of np.load
+    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+    # call load_data with allow_pickle implicitly set to true
+
+
+    (input_train, y_train), (input_test, y_test) = imdb.load_data(path='/home/wb/baidunetdiskdownload/imdb.npz',num_words=max_features)
+    # may or may not need this line: word_index=imdb.get_word_index(path="E:/ML/TextClassification/imdb_word_index.json")
+    ## restore np.load for future normal usage
+    #np.load = np_load_old
+
+
+    print(len(input_train), 'train sequences')
+    print(len(input_test), 'test sequences')
+    print('Pad sequences (samples x time)')
+    input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+    input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
+    print('input_train shape:', input_train.shape)
+    print('input_test shape:', input_test.shape)
+    #### end of loading imdb ####
+
+
+
+
+    # Load data
+    #(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+    x_train = input_train
+    x_test = input_test
+    y_train = y_train
+    y_test = y_test
+
+    # Reverse sequences
+    x_train = [x[::-1] for x in x_train]
+    x_test = [x[::-1] for x in x_test]
+
+    # Pad sequences
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+    model = Sequential()
+    model.add(layers.Embedding(max_features, 128))
+    model.add(layers.LSTM(32))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+    history = model.fit(x_train, y_train,
+                        epochs=10,
+                        batch_size=128,
+                        validation_split=0.2)
+    
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
 
